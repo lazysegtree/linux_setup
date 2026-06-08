52bfbae3214e7124f6c61578ae6b8804d24cf152
lazysegtree <59679977+lazysegtree@users.noreply.github.com>
2026-06-08 07:55:19 +0530

    chore: Nvim clipboard setup and additional instructions

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,8 @@
 require("config.lazy")
 
+-- system clipboard
+vim.opt.clipboard = "unnamedplus"
+
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
