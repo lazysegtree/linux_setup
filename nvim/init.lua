69f4b71b3fb4b2428ab4a756ab24ab197ae2b4b8
Nitin Kumar <59679977+lazysegtree@users.noreply.github.com>
2026-07-22 11:03:54 +0530

    fix: vscode like insert mode typing

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,3 +24,17 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.whichwrap:append(">")
 vim.opt.whichwrap:append("<")
+
+-- Interpret the existing iTerm2 shortcuts like zsh does while in Insert mode.
+local function imap(lhs, rhs, description)
+  vim.keymap.set("i", lhs, rhs, {
+    silent = true,
+    desc = description,
+  })
+end
+
+imap("<M-b>", "<C-G>U<C-Left>", "Move one word left")
+imap("<M-f>", "<C-G>U<C-Right>", "Move one word right")
+imap("<M-BS>", "<C-W>", "Delete previous word")
+imap("<C-A>", "<C-G>U<Home>", "Move to beginning of line")
+imap("<C-E>", "<C-G>U<End>", "Move to end of line")
